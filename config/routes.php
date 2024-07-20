788f351a14f800f85939349029e917f0eabc85ff
moehakram <muh.akram1422@gmail.com>
2024-07-21 02:59:06 +0800

    add file config routes

---
+++ config/routes.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'POST' => [
+        '/users/login'      => 'auth@login', // file_name:function_name
+        '/users/register'   => 'auth@register',
+    ],
+    'GET' => [
+        '/'                 => 'home@index',
+        '/home'             => 'home@home',
+        '/users/login'      => 'auth@showLogin',
+        '/users/register'   => 'auth@showRegister',
+        '/users/activate'   => 'remember_me@activate',
+        '/users/logout'     => 'auth@logout',
+        '/tes/(\d+)'        => 'home@testing',
+        '/tes/(\w+)'        => 'home@testing',
+    ]
+];
