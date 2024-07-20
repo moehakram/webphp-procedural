d41daf004ffc9d9efaccfdfe026336be2fd42f24
moehakram <muh.akram1422@gmail.com>
2024-07-20 15:47:02 +0800

    refactoring

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $title ?? 'Home' ?></title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap">
-    <link rel="stylesheet" href="./assets/styles.css">
+    <link rel="stylesheet" href="/assets/styles.css">
 </head>
 <body>
     <main>
